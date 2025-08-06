8ca34193b992efa99b62fcc5a56aac368fc6024c
petrius <maria.petrius@gmail.com>
2025-08-06 17:32:39 +0200

    fix problem with refresh page and display data

--- src/app/components/payment-monthly-summary/payment-monthly-summary.ts
+++ src/app/components/payment-monthly-summary/payment-monthly-summary.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PaymentService } from '../../services/payment-service';
 import { MonthlySummary } from '../../models/MonthlySummary';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-payment-monthly-summary',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './payment-monthly-summary.html',
   styleUrl: './payment-monthly-summary.css'
 })
@@ -13,25 +14,36 @@ export class PaymentMonthlySummary implements OnInit {
 
   monthlySummaries: MonthlySummary[] = [];
   errorMessage = "";
+  isLoading = false;
 
   tableColumns = [
-    {property: "yearMonth", label: "Year-Month"},
+    {property: "yearMonth", label: "Year - Month"},
     {property: "totalAmount", label: "Total Paid"},
   ]
 
   constructor(
-    private paymentService: PaymentService
+    private paymentService: PaymentService,
   ){}
 
   ngOnInit(): void{
-      this.paymentService.getPaymentMonthlySummary().subscribe({
-        next: (monthlySummary) => {
-          console.log(monthlySummary)
-          this.monthlySummaries = monthlySummary;
-        },
-        error: (error) => {
-          this.errorMessage = error.error?.errorMessage || "api is failed"
-        }
-      });
+
+    this.isLoading = true;
+    this.paymentService.getPaymentMonthlySummary().subscribe({
+      next: (monthlySummaries) => {
+        console.log('Data received:', monthlySummaries);
+        console.log('Array length:', monthlySummaries.length); // Add this
+        console.log('First item:', monthlySummaries[0]);
+        this.monthlySummaries = monthlySummaries;
+        console.log('tim', this.monthlySummaries);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = error.error?.errorMessage || "api is failed"
+        this.isLoading = false;
+      },
+      complete: () => {
+        console.log('🏁 HTTP request completed');
+      }
+    });
   }
 }
